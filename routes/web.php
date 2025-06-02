eced97a0e274923ca1275bdd45fbd05aacdd4e05
Vince <jpinard@bluewin.ch>
2025-06-02 14:47:14 +0200

    API Routes group creation

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,20 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+    // Frontend Routes
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+
+    //API Routes
+    Route::prefix('api/v1')->group(function () {
+        Route::get('/test', function () {
+            return response()->json(['message' => 'Test API endpoint']);
+        });
+    });
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';

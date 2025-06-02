f2d3e5dc6137e2af0bf99b1af9993fccc3b60a89
Vince <jpinard@bluewin.ch>
2025-06-02 16:18:54 +0200

    API routes work in progress

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\League\ArenaController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\League\RankingController;
+use App\Http\Controllers\League\QuizController;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -18,6 +21,24 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::prefix('api/v1')->group(function () {
         Route::get('/test', function () {
             return response()->json(['message' => 'Test API endpoint']);
+        });
+
+        Route::prefix('ranking')->group(function () {
+            Route::get('/global', [RankingController::class, 'getGlobalRanking'])->name('');
+            Route::get('/league', [RankingController::class, 'getLeagueRanking'])->name('');
+            Route::get('/pos', [RankingController::class, 'getPosRanking'])->name('');
+        });
+
+        Route::prefix('quiz')->group(function () {
+            Route::get('/', [QuizController::class, 'getNewQuiz'])->name('');
+            Route::get('/battle/{quiz-id}', [QuizController::class, 'getBattleQuizById'])->name('');
+            Route::get('/placement', [QuizController::class, 'getPlacementQuiz'])->name('');
+        });
+
+        Route::prefix('novelties')->group(function () {
+            Route::get('/', [ArenaController::class, 'isNoveltyOccuring'])->name('');
+            Route::get('/arena', [ArenaController::class, 'getArenaQuiz'])->name('');
+            Route::get('/results', [ArenaController::class, 'getArenaResults'])->name('');
         });
     });
 });

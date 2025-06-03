34f4705bde83a22e816b32c21dcdab1b40a8da6c
Vince <jpinard@bluewin.ch>
2025-06-03 09:20:21 +0200

    API routes work in progress

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\League\ArenaController;
+use App\Http\Controllers\League\BattleController;
+use App\Http\Controllers\League\ImagesController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\League\RankingController;
 use App\Http\Controllers\League\QuizController;
+use App\Http\Controllers\League\UserController;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -31,14 +34,29 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
         Route::prefix('quiz')->group(function () {
             Route::get('/', [QuizController::class, 'getNewQuiz'])->name('');
-            Route::get('/battle/{quiz-id}', [QuizController::class, 'getBattleQuizById'])->name('');
             Route::get('/placement', [QuizController::class, 'getPlacementQuiz'])->name('');
+            Route::post('/response', [QuizController::class, 'saveAnswer'])->name('');
+
+            Route::prefix('battle')->group(function () {
+                Route::get('/{quiz-id}', [BattleController::class, 'getBattleQuiz'])->name('');
+                Route::post('/bet', [BattleController::class, 'placeBetForBattle'])->name('');
+            });
         });
 
         Route::prefix('novelties')->group(function () {
             Route::get('/', [ArenaController::class, 'isNoveltyOccuring'])->name('');
-            Route::get('/arena', [ArenaController::class, 'getArenaQuiz'])->name('');
+            Route::get('/arena-quiz', [ArenaController::class, 'getArenaQuiz'])->name('');
             Route::get('/results', [ArenaController::class, 'getArenaResults'])->name('');
+        });
+
+        Route::prefix('user')->group(function () {
+            Route::get('/', [UserController::class, 'isNoveltyOccuring'])->name('');
+            Route::get('/history', [UserController::class, 'getArenaQuiz'])->name('');
+            Route::get('/placement-progress', [UserController::class, 'getArenaResults'])->name('');
+        });
+
+        Route::prefix('images')->group(function () {
+            Route::get('/{image_path}', [ImagesController::class, 'getImage'])->name('');
         });
     });
 });
